c2e89605b6c54df6d9f3ff7f2c885afd2ab60ac5
JEFERSON TRINDADE SANTANA <jefersonsantana.ts@gmail.com>
2025-06-02 17:40:22 -0300

    Committing 6 items from workspace 17a9574a-6115-455f-aa5d-2ad85a3b59c1

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,21 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "bf67ecec-cda5-419e-bf55-62a2f934944d",
+# META       "default_lakehouse_name": "lake",
+# META       "default_lakehouse_workspace_id": "17a9574a-6115-455f-aa5d-2ad85a3b59c1",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "bf67ecec-cda5-419e-bf55-62a2f934944d"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
